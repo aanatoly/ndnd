4e4c2c9dc0d71299529fe67abbb18e62ca36eae3
Anatoly <aanatoly@gmail.com>
2015-03-19 15:18:25 +0200

    create gh-pages branch

--- test/test_click.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-pid=$(xprop -root | grep NDND_PID | sed -e 's/.*=//')
-echo $pid
-
-while true; do
-    sleep 2
-    kill -USR1 $pid
-done
--- test/test_click_toggle.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-pid=$(xprop -root | grep NDND_PID | sed -e 's/.*=//')
-
-sleep 1
-kill -USR1 $pid
